1bb324c9d15be09217b157a2105651327849a5aa
Shahan Khan <shahan.ali.khan.fiem@gmail.com>
2025-08-19 23:15:12 +0530

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,10 +5,6 @@ import logo from "../assets/logo.png";
 const USERNAME = process.env.REACT_APP_USERNAME;
 const PASSWORD = process.env.REACT_APP_PASSWORD;
 
-console.log(USERNAME, PASSWORD);
-
-console.log(USERNAME, PASSWORD);
-
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
